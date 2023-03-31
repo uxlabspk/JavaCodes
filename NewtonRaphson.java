7eaa15f71387808e1342b8452d9b5f26af2a76b6
Muhammad Naveed <85978999+uxlabspk@users.noreply.github.com>
2023-03-31 15:20:08 +0500

    initial release

---
+++ NewtonRaphson.java
@@ -0,0 +1,66 @@
+
+// Muhammad Naveed.
+// https://twitter.com/prog_naveed/
+// Finding the square root using newton raphson method.
+
+
+import java.util.Scanner;
+
+public class NewtonRaphson 
+{
+    private int number;
+    private float[] root;
+    private int initialGuess;
+
+    // Default constructor for initialising variables.
+    public NewtonRaphson() 
+    {
+        initialGuess = 1;
+        number = 0;
+        root = new float[100];
+    }
+
+    // setter method
+    public void setNumber(int number) 
+    {
+        this.number = number;
+    }
+
+    // square root finder method
+    public void calculateRoot() 
+    {
+        root[0] = initialGuess;
+        for (int i = 0; i < root.length; i++) 
+        {
+            // newton raphson algorithm.
+            root[i + 1] = (float) (0.5) * (root[i] + (number / root[i]));
+
+            if (root[i] == root[i + 1]) 
+            {
+                showResults(root[i], i);
+                break;
+            } 
+            else 
+            {
+                continue;
+            }
+        }
+    }
+
+    // Result showing constant method.
+    public final void showResults(float result, int i) 
+    {
+        System.out.println("The square root of " + number + " is " + result + " and found in " + i + "th iteration.");
+    }
+
+    public static void main(String[] args) 
+    {
+        var NewtonRaphson_obj = new NewtonRaphson();
+        var s = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        NewtonRaphson_obj.setNumber(s.nextInt());
+        s.close();
+        NewtonRaphson_obj.calculateRoot();
+    }
+}
